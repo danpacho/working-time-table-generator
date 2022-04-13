f77dfef42b0017333ec5400ede598df7f84ef493
June@MacBook <danpa725@cau.ac.kr>
2022-04-13 21:48:25 +0900

    Add: api constants for request .

---
+++ src/constants/api.ts
@@ -0,0 +1,27 @@
+const API_URL = "/getRestDeInfo";
+
+export interface API_PARAM_CONFIG {
+    solYear?: number;
+    solMonth?: number;
+    ServiceKey: string;
+    _type: "json" | "xml";
+    numOfRows: number;
+}
+
+export interface API_RESPONSE {
+    response: {
+        body: {
+            items: {
+                item: {
+                    locdate: number;
+                    seq: number;
+                    dateKind: number;
+                    isHolliday: "Y";
+                    dateName: string;
+                }[];
+            };
+        };
+    };
+}
+
+export { API_URL };
