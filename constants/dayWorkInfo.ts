772322d21abd2b14ce88575cb8c5ee1414d54706
June@MacBook <danpa725@cau.ac.kr>
2022-06-30 22:10:21 +0900

    Refactor: constants folder location .

--- constants/dayWorkInfo.ts
+++ constants/dayWorkInfo.ts
@@ -8,32 +8,25 @@ const DAY_WORK_INFO: {
     workIndex: number;
 }[] = [
     {
-        time: "9:00 ~ 10:30",
-        workHour: "1시간 30분",
-        color: "teal",
-        borderColor: "teal5",
-        workIndex: 0,
-    },
-    {
         time: "10:30 ~ 12:30",
         workHour: "2시간",
         color: "red",
         borderColor: "red3",
-        workIndex: 1,
+        workIndex: 0,
     },
     {
         time: "13:30 ~ 15:15",
         workHour: "1시간 45분",
         color: "indigo",
         borderColor: "blue3",
-        workIndex: 2,
+        workIndex: 1,
     },
     {
         time: "15:15 ~ 17:00",
         workHour: "1시간 45분",
         color: "indigo",
         borderColor: "blue3",
-        workIndex: 3,
+        workIndex: 2,
     },
 ];
 
