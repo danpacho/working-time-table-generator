f211e7bb26ffac44116df5ef9470ed23f561afb8
June@MacBook <danpa725@cau.ac.kr>
2022-07-04 14:35:03 +0900

    Update: constant type as const .

--- src/constants/localStorageKey.ts
+++ src/constants/localStorageKey.ts
@@ -1,4 +1,4 @@
-const AGENT_LIST_KEY = "AGENT_LIST";
-const WORK_INFO_ARRAY_KEY = "WORK_INFO";
+const AGENT_LIST_KEY = "AGENT_LIST" as const;
+const WORK_INFO_ARRAY_KEY = "WORK_INFO" as const;
 
 export { AGENT_LIST_KEY, WORK_INFO_ARRAY_KEY };
