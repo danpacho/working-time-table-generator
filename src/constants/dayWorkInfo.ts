53e686c2bd0113bad92656478fa434c79ed79356
June@MacBook <danpa725@cau.ac.kr>
2022-07-04 14:30:54 +0900

    Update: work info constant .

--- src/constants/dayWorkInfo.ts
+++ src/constants/dayWorkInfo.ts
@@ -1,53 +1,37 @@
-import { PalleteType } from "@styles/utils/pallete";
+import type { PalleteType } from "@styles/utils/pallete";
 
 type WorkType = "access-control" | "shuttle-operation";
 const DAY_WORK_INFO: {
-    time: string;
+    time: string | string[];
     workHour: string;
     color: string;
     borderColor: PalleteType;
-    workIndex: number;
+    workOrder: number;
     workType: WorkType;
 }[] = [
     {
-        time: "09:00 ~ 10:45",
-        workHour: "1시간 45분",
-        color: "indigo",
-        borderColor: "blue2",
-        workIndex: 0,
-        workType: "access-control",
-    },
-    {
-        time: "09:30 ~ 11:30",
-        workHour: "2시간",
-        color: "teal",
-        borderColor: "teal5",
-        workIndex: 1,
-        workType: "shuttle-operation",
-    },
-    {
-        time: "10:45 ~ 12:30",
-        workHour: "1시간 45분",
-        color: "indigo",
-        borderColor: "blue4",
-        workIndex: 2,
-        workType: "access-control",
-    },
-    {
-        time: "13:30 ~ 15:30",
-        workHour: "2시간",
-        color: "teal",
-        borderColor: "teal5",
-        workIndex: 3,
-        workType: "shuttle-operation",
-    },
-    {
-        time: "13:30 ~ 17:00",
+        time: ["09:00 ~ 10:45", "13:30 ~ 15:15"],
         workHour: "3시간 30분",
-        color: "red",
-        borderColor: "red4",
-        workIndex: 4,
+        color: "indigo",
+        borderColor: "blue3",
+        workOrder: 0,
         workType: "access-control",
+    },
+    {
+        time: ["10:45 ~ 12:30", "15:15 ~ 17:00"],
+        workHour: "3시간 30분",
+        color: "indigo",
+        borderColor: "blue3",
+        workOrder: 1,
+        workType: "access-control",
+    },
+    {
+        time: ["09:30 ~ 11:30", "13:30 ~ 15:30"],
+        workHour: "4시간",
+        color: "teal",
+        borderColor: "teal4",
+        workOrder: 2,
+        workType: "shuttle-operation",
     },
 ];
 
