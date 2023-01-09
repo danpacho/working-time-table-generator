0754314d760d8d2d9a93e3de7d8a1a2f46f930c1
Danpa_cho <danpa725@cau.ac.kr>
2023-01-09 13:53:29 +0900

    feat: 요원 근무 일정 수정

--- src/constants/dayWorkInfo.ts
+++ src/constants/dayWorkInfo.ts
@@ -1,5 +1,6 @@
 import type { PalletType } from "@styles/utils/pallet";
 
+//!important 반드시 workOrder를 수정 해야한다...
 type WorkType = "access-control" | "shuttle-operation";
 const DAY_WORK_INFO: {
     time: string | string[];
@@ -10,7 +11,7 @@ const DAY_WORK_INFO: {
     workType: WorkType;
 }[] = [
     {
-        time: ["09:00 ~ 10:45", "13:30 ~ 15:15"],
+        time: ["09:00 ~ 12:30"],
         workHour: "3시간 30분",
         color: "indigo",
         borderColor: "blue3",
@@ -18,19 +19,11 @@ const DAY_WORK_INFO: {
         workType: "access-control",
     },
     {
-        time: ["10:45 ~ 12:30", "15:15 ~ 17:00"],
-        workHour: "3시간 30분",
-        color: "indigo",
-        borderColor: "blue3",
-        workOrder: 1,
-        workType: "access-control",
-    },
-    {
         time: ["09:30 ~ 11:30", "13:30 ~ 15:00"],
         workHour: "3시간 30분",
         color: "teal",
         borderColor: "teal5",
-        workOrder: 2,
+        workOrder: 1,
         workType: "shuttle-operation",
     },
 ];
